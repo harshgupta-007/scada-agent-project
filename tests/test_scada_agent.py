bc8bf882423a357409b4691d7eb741f1c0f01b52
Harsh Gupta <harshg889654@gmail.com>
2026-04-06 13:02:31 +0530

    clean project structure

--- tests/test_scada_agent.py
+++
@@ -1,8 +0,0 @@
-from scada_summary_agent.agent import root_agent
-
-def test_agent():
-    response = root_agent.run(
-        "Summarize this SCADA file.",
-        file_path="scada_summary_agent/sample_scada.csv"
-    )
-    print(response)
